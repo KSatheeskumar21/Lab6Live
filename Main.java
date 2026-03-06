13e33f1bdfa86644de67725566929b390404a79e
Kishore Satheeskumar <kishore.satheeskumar@gmail.com>
2026-03-06 14:16:53 +0000

    Added printHundred()

--- Main.java
+++ Main.java
@@ -2,4 +2,10 @@ class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+
+    public static void printHundred() {
+        for (int i=1; i<=100; i++) {
+            System.out.println(i);
+        }
+    }
 }
